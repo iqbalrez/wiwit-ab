67da25d55b055f683392d617547b63e6c6b24e80
Aldyo Putra <106857082+dyooo123@users.noreply.github.com>
2023-10-03 15:29:50 +0700

    add : footer

---
+++ resources/views/user/layouts/footer.blade.php
@@ -0,0 +1,116 @@
+<footer class="bg-white" aria-labelledby="footer-heading">
+    <h2 id="footer-heading" class="sr-only">Footer</h2>
+    <div class="px-5 py-12 mx-auto max-w-7xl lg:py-16 md:px-12 lg:px-20">
+        <div class="xl:grid xl:grid-cols-3 xl:gap-8">
+            <p class="mt-2 text-sm font-normal text-[#757575]">Lorem, ipsum dolor sit amet consectetur adipisicing
+                elit. Numquam asperiores enim atque voluptas dolorum unde exercitationem neque nostrum consequuntur
+                .</p>
+            <div class="grid grid-cols-2 gap-8 mt-12 xl:mt-0 xl:col-span-2">
+                <div class="md:grid md:grid-cols-2 md:gap-8">
+                    <div>
+                        <h3 class="font-semibold text-base text-black ">
+                            Overview
+                        </h3>
+                        <ul role="list" class="mt-4 space-y-3">
+                            <li>
+                                <a href="#" class="text-sm font-normal text-[#133D4F] mt-6 hover:text-green-700">
+                                    Home
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" class="text-sm font-normal text-[#133D4F] mt-5 hover:text-green-700">
+                                    Portofolio
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" class="text-sm font-normal text-[#133D4F] mt-5 hover:text-green-700">
+                                    Events
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" class="text-sm font-normal text-[#133D4F] mt-5 hover:text-green-700">
+                                    Material
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" class="text-sm font-normal text-[#133D4F]  mt-5 hover:text-green-700">
+                                    Contact Me
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" class="text-sm font-normal text-[#133D4F] mt-5 hover:text-green-700">
+                                    Consultation
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="mt-12 md:mt-0">
+                        <ul role="list" class="mt-10 space-y-4">
+                            <li>
+                                <a href="#" class="text-sm text-[#133D4F] hover:text-green-700">
+                                    Offices
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" class="text-sm font-normal text-[#133D4F] mt-5 hover:text-green-700">
+                                    Careers
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" class="text-sm font-normal text-[#133D4F] mt-5 hover:text-green-700">
+                                    FAQs
+                                </a>
+                            </li>
+
+                        </ul>
+                    </div>
+                </div>
+                <div class="hidden lg:justify-end md:grid md:grid-cols-1">
+                    <div class="w-full mt-12 md:mt-0">
+                        <div class="mt-8 lg:justify-end xl:mt-0">
+                            <h3 class="font-semibold text-sm text-black ">
+                                Business Info
+                            </h3>
+                            <p class="mt-4 text-sm font-normal text-[#133D4F] lg:ml-auto">
+                                Pogung Dalangan, Sinduadi, Kec. Mlati, Kabupaten Sleman, DIY
+                            </p>
+                            <div class="w-full mt-9 text-center py-3 bg-[#FAFBFD] font-normal rounded-md text-sm border-solid border-2  h-12 text-[#133D4F] border-[#F2F2F2]">
+                                06287834671064
+                            </div>
+                            <div class="w-full mt-5 text-center py-3 bg-[#FAFBFD] font-normal rounded-md border-solid  border-2 text-sm h-12 text-[#133D4F] border-[#F2F2F2]">
+                              wiwitab@gmail.com
+                          </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="py-12 mx-auto border-t text-center max-w-7xl  sm:px-6 md:md:items-center md:justify-between lg:px-20">
+        <div class="mb-8 space-x-6 flex justify-center md:order-last md:mb-0">
+            <span class="gap-3 mx-auto">
+                <a href="#" class="w-6 h-6 px-2 transition text-[#435660] hover:text-green-700">
+                    <span class="sr-only">Facebook</span>
+                    <ion-icon class="w-5 h-5 md hydrated" name="logo-facebook" role="img"
+                        aria-label="logo facebook"></ion-icon>  
+                </a>
+                <a href="#"class="w-6 h-6 px-2 transition text-[#435660] hover:text-green-700">
+                    <span class="sr-only">Youtube</span>
+                    <ion-icon class="w-5 h-5 md hydrated" name="logo-youtube" role="img"
+                        aria-label="logo youtube"></ion-icon>
+                </a>
+                <a href="#"class="w-6 h-6 px-2 transition text-[#435660] hover:text-green-700">
+                    <span class="sr-only">Instagram</span>
+                    <ion-icon class="w-5 h-5 md hydrated" name="logo-instagram" role="img"
+                        aria-label="logo instagram"></ion-icon>
+                </a>
+                <a href="#"class="w-6 h-6  px-2 transition text-[#435660] hover:text-green-700">
+                    <span class="sr-only">Linkedin</span>
+                    <ion-icon class="w-5 h-5 md hydrated" name="logo-linkedin" role="img"
+                        aria-label="logo linkedin"></ion-icon>
+                </a>
+            </span>
+            <a href="#" class="text-[#133D4F] font-normal inline-flex text-sm text-right hover:text-green-700">Back To Top</a>
+        </div>
+    </div>
+</footer>
