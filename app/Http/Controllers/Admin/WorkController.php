2cc4ad945a827817b7ff3b89834665d4f8f350d3
Iqbalrez <iqbalrr02@gmail.com>
2023-10-09 11:32:04 +0700

    add: backend work

--- app/Http/Controllers/Admin/WorkController.php
+++ app/Http/Controllers/Admin/WorkController.php
@@ -3,12 +3,77 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Interfaces\WorkCategoryInterface;
+use App\Interfaces\WorkInterface;
 use Illuminate\Http\Request;
 
 class WorkController extends Controller
 {
+    private $work;
+    private $workCategory;
+
+    public function __construct(WorkInterface $work, WorkCategoryInterface $workCategory)
+    {
+        $this->work         = $work;
+        $this->workCategory = $workCategory;
+    }
+
     public function index()
     {
-        return view('admin.work.index');
+        return view('admin.work.index', [
+            'works'          => $this->work->getAll(),
+            'workCategories' => $this->workCategory->getAll(),
+        ]);
     }
+
+    public function show($id)
+    {
+        return response()->json($this->work->getById($id));
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'work_category_id' => ['required'],
+            'title'            => ['required'],
+            'date'      => ['required'],
+            'photo'        => ['required', 'mimes:jpg,jpeg,png']
+        ]);
+
+        try {
+            $this->work->store($request->all());
+            return redirect()->back()->with('success', 'Work created successfully');
+        } catch (\Throwable $th) {
+            return redirect()->back()->with('error', $th->getMessage());
+        }
+    }
+
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'work_category_id' => ['required'],
+            'title'            => ['required'],
+            'date'      => ['required'],
+            'photo'        => ['mimes:jpg,jpeg,png'],
+        ]);
+
+        try {
+            $this->work->update($id, $request->all());
+            return redirect()->back()->with('success', 'Work updated successfully');
+        } catch (\Throwable $th) {
+            return redirect()->back()->with('error', $th->getMessage());
+        }
+    }
+
+    public function destroy($id)
+    {
+        try {
+            $this->work->destroy($id);
+            return redirect()->back()->with('success', 'Work deleted successfully');
+        } catch (\Throwable $th) {
+            return redirect()->back()->with('error', $th->getMessage());
+        }
+    }
+
+    
 }
