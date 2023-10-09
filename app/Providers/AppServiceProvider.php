51e134966b8e5629108b9d529d91705637dc2194
Aldyo Putra <106857082+dyooo123@users.noreply.github.com>
2023-10-09 11:00:53 +0700

    edit : app service

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -12,6 +12,12 @@ class AppServiceProvider extends ServiceProvider
         $this->app->bind(\App\Interfaces\MessageInterface::class, \App\Repositories\MessageRepository::class);
         $this->app->bind(\App\Interfaces\SocialMediaInterface::class, \App\Repositories\SocialMediaRepository::class);
         $this->app->bind(\App\Interfaces\TestimonialInterface::class, \App\Repositories\TestimonialRepository::class);
+        $this->app->bind(\App\Interfaces\BlogCategoryInterface::class, \App\Repositories\BlogCategoryRepository::class);
+        $this->app->bind(\App\Interfaces\BlogInterface::class, \App\Repositories\BlogRepository::class);
+        $this->app->bind(\App\Interfaces\MaterialCategoryInterface::class, \App\Repositories\MaterialCategoryRepository::class);
+        $this->app->bind(\App\Interfaces\MaterialInterface::class, \App\Repositories\MaterialRepository::class);
+        $this->app->bind(\App\Interfaces\WorkCategoryInterface::class, \App\Repositories\WorkCategoryRepository::class);
+        $this->app->bind(\App\Interfaces\EventCategoryInterface::class, \App\Repositories\EventCategoryRepository::class);
     }
 
     /**
