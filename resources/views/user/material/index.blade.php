0340a2809e04dc96cf1404f82f16cc0b25c182a8
Iqbalrez <iqbalrr02@gmail.com>
2023-11-01 10:14:26 +0700

    edit: ratio thumbnail

--- resources/views/user/material/index.blade.php
+++ resources/views/user/material/index.blade.php
@@ -81,7 +81,7 @@
                                 <div class="bg-white shadow-sm p-4 rounded-xl border border-gray-100"
                                     onclick="download('{{ $data->id }}')">
                                     <div class="flex gap-3 items-start">
-                                        <div class="w-28 h-28 aspect-square">
+                                        <div class="w-28 h-28 aspect-square object-cover">
                                             <img src={{ $data->thumbnail ? asset('storage/materials/' . $data->thumbnail) : asset('assets/default.png') }}
                                                 class="hidden border border-gray-100 md:block object-center object-cover rounded-lg bg-gray-50 mr-4"
                                                 alt="">
