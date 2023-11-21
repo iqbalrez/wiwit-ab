0fafe52d5d56bd759ecd36d5019bb5f34ec616b6
Iqbalrez <iqbalrr02@gmail.com>
2023-11-21 12:49:57 +0700

    refactor blog detail view layout

--- resources/views/user/blog/detail.blade.php
+++ resources/views/user/blog/detail.blade.php
@@ -6,14 +6,14 @@
                 class="flex items-center justify-center w-full px-5 pt-20 py-20 mx-auto md:px-12 lg:px-14 xl:px-0 max-w-6xl">
                 <div class="relative flex-col items-start m-auto align-middle">
                     <div class="relative items-center m-auto lg:inline-flex sm:order-first col-span-2">
-                        <div class="text-center lg:text-center">
+                        <div class="text-center lg:text-center max-w-2xl">
                             <div>
                                 <p class="2xl:text-sm  text-primary font-semibold">
                                     {{ $blog->blogCategory->title }}
                                 </p>
-                                <p class="mb-8 text-2xl font-semibold tracking-tight text-dark sm:text-xl">
+                                <h1 class="mb-8 text-xl font-semibold tracking-tight text-dark md:text-2xl">
                                     {{ $blog->title }}
-                                </p>
+                                </h1>
                                 <p class="text-sm font-semibold text-dark">
                                     {{ $blog->author }}
                                 </p>
@@ -30,18 +30,20 @@
 
         <section class="max-w-4xl mx-auto">
             <div class="px-8 mx-auto transform -translate-y-20 sm:-translate-y-14">
-                <img class="rounded-xl w-full ces mx-auto md:max-h-[500px] object-cover object-center"
+                <img id="main-image" class="rounded-xl w-full ces mx-auto md:max-h-[500px] object-cover object-center"
                     src="{{ $blog->thumbnail ? asset('storage/blogs/thumbnail/' . $blog->thumbnail) : asset('assets/default.png') }}"
                     alt="">
             </div>
 
-            <div class="max-w-4xl mx-auto px-8 mt-6">
-                <p class="font-normal leading-relaxed text-justify tracking-tight text-sm 2xl:text-md text-black">
+            <div class="max-w-4xl mx-auto px-8 mt-4">
+                <div class="leading-relaxed text-justify -tracking-tight text-sm 2xl:text-md"
+                    style="text-align: justify">
                     {!! $blog->content !!}
-                </p>
+                </div>
             </div>
 
-            <div class="flex items-center justify-center px-8 w-full mt-6 mx-auto md:px-12 lg:px-14 xl:px-0 max-w-4xl">
+            <div
+                class="mb-8 md:mb-14 flex items-center justify-center px-8 w-full mt-6 mx-auto md:px-12 lg:px-14 xl:px-0 max-w-4xl">
                 <div class="flex items-center justify-center mt-14">
                     <a href="{{ route('blog.index') }}"
                         class="flex items-center justify-center text-sm px-8 py-3.5 mt-4 md:mt-0 rounded-full text-grey-500 bg-gray-200 hover:bg-gray-300 focus:outline-none focus:text-dark">
@@ -50,4 +52,17 @@
                 </div>
             </div>
         </section>
+
+        @push('js-internal')
+            <script>
+                $(document).ready(function() {
+                    // remove style in image except main image
+                    $('img').not('#main-image').removeAttr('style');
+                    // style img in content
+                    $('img').not('#main-image, .related-image').addClass(
+                        'blur-mode my-4 w-[40rem] h-full mx-auto object-cover object-center border border-gray-200 rounded-xl'
+                    );
+                });
+            </script>
+        @endpush
 </x-guest-layout>
