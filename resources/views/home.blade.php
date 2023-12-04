1c7d5e9395a1eae56ff581279eb8ab9c4f4e10f6
Iqbalrez <iqbalrr02@gmail.com>
2023-12-04 16:55:53 +0700

    Refactor partner section in home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -183,23 +183,21 @@
             </div>
 
             {{-- Partners --}}
-            <section class="py-12">
-                <div class="">
-                    <div class="px-4 mx-auto max-w-6xl sm:px-6 lg:py-8 lg:px-8" data-aos="fade-up">
-                        <div class="mt-6 grid gap-5 md:grid-flow-row lg:mt-8 swiper-container">
-                            <div class="swiper-wrapper">
-                                @foreach ($partners as $partner)
-                                    <div class="swiper-slide flex justify-center col-span-1">
-                                        <img class="blur-mode w-full h-20 object-contain"
-                                            src="{{ asset('storage/partner/' . $partner->logo) }}" alt="Partner Logo" />
-                                    </div>
-                                @endforeach
-                            </div>
-                            <div class="swiper-pagination"></div>
+            <div class="">
+                <div class="px-4 mx-auto max-w-6xl sm:px-6 lg:py-8 lg:px-8" data-aos="fade-up">
+                    <div class="mt-6 grid gap-5 md:grid-flow-row lg:mt-8 swiper-container">
+                        <div class="swiper-wrapper">
+                            @foreach ($partners as $partner)
+                                <div class="swiper-slide flex justify-center col-span-1">
+                                    <img class="blur-mode w-full h-20 object-contain"
+                                        src="{{ asset('storage/partner/' . $partner->logo) }}" alt="Partner Logo" />
+                                </div>
+                            @endforeach
                         </div>
+                        <div class="swiper-pagination"></div>
                     </div>
                 </div>
-            </section>
+            </div>
     </section>
 
     @push('js-internal')
